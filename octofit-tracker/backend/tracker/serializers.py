793fce42b20ed94b2b6b32e55557ffae14e9672a
sblackle <svenblackley@gmail.com>
2026-02-04 11:34:23 +0000

    Update serializers to convert ObjectId fields to strings

---
+++ octofit-tracker/backend/tracker/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from .models import User, Team, Activity, Leaderboard, Workout
+from bson import ObjectId
+
+
+class ObjectIdField(serializers.Field):
+    def to_representation(self, value):
+        return str(value)
+
+    def to_internal_value(self, data):
+        return ObjectId(data)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    members = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+
+class ActivitySerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    user = UserSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), source='user', write_only=True)
+
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    team = TeamSerializer(read_only=True)
+    team_id = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all(), source='team', write_only=True)
+
+    class Meta:
+        model = Leaderboard
+        fields = '__all__'
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    user = UserSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), source='user', write_only=True)
+
+    class Meta:
+        model = Workout
+        fields = '__all__'
